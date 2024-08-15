eaf1a7e509eaac7e8a9b9b6aeb9ee20bb1ea92b4
MariadeJesus-prog <marydejesushernandez72@gmail.com>
2024-08-15 08:09:08 -0600

    Update QRcode.js

--- QRcode.js
+++ QRcode.js
@@ -1,18 +1,20 @@
-var url = ventana.ubicacion;
-//var x = url.indexOf("https://");
+var url = window.location;
+
+// Obtener parámetros QRW y QRH
 var QRWValue = getUrlParameter("QRW");
 var QRHValue = getUrlParameter("QRH");
-si (url.protocolo == "http:") {
 
-    documento.write("<iframe id='Iframe1' src='http://dunsregistered.dnb.com/SealAuthentication.aspx?Cid=1&QR=1&QRW=" + QRWValue + "&QRH=" + QRHValue + "' frameborder='0' scrolling='no' allowtransparency='true' ></iframe>");
+// Determinar si la URL es HTTP o HTTPS y crear el iframe apropiado
+if (url.protocol === "http:") {
+    document.write("<iframe id='Iframe1' src='http://dunsregistered.dnb.com/SealAuthentication.aspx?Cid=1&QR=1&QRW=" + QRWValue + "&QRH=" + QRHValue + "' frameborder='0' scrolling='no' allowtransparency='true'></iframe>");
+} else {
+    document.write("<iframe id='Iframe1' src='https://dunsregistered.dnb.com/SealAuthentication.aspx?Cid=1&QR=1&QRW=" + QRWValue + "&QRH=" + QRHValue + "' frameborder='0' scrolling='no' allowtransparency='true'></iframe>");
 }
-demás {
-    documento.write("<iframe id='Iframe1' src='https://dunsregistered.dnb.com/SealAuthentication.aspx?Cid=1&QR=1&QRW=" + QRWValue + "&QRH=" + QRHValue + "' frameborder='0' scrolling='no' allowtransparency='true' ></iframe>");
 
+// Función para obtener parámetros de la URL
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(window.location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
-función getUrlParameter(nombre) {
-    nombre = nombre.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + nombre + '=([^&#]*)');
-    var resultados = regex.exec(document.currentScript.src);
-    devolver resultados === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
